c0d5cfd05a130e07e8e2cc9168c602ff71637f24
kamila-pie <kamilapietraszuk@gmail.com>
2020-08-19 00:59:53 +0200

    fixing style

--- powerplan/src/components/plans/CreatePlan.js
+++ powerplan/src/components/plans/CreatePlan.js
@@ -25,15 +25,40 @@ class CreatePlan extends Component {
             <div className="container">
                 <Form className="plan-form" onSubmit={this.handleSubmit} >
                     <h2>Let's create new plan</h2>
-                    <div>
-                        <Label htmlFor={'title'}>Title</Label>
-                        <Input type={'text'} id={'title'} onChange={this.handleChange} placeholder={'name your plan'}/>
-                    </div>
-                    <FormGroup>
-                        <Label>Plan Content</Label>
-                        <textarea id="content" onChange={this.handleChange} placeholder={'content'}/>
+                    <FormGroup className="createPlanWrapper">
+                        <div className={"newPlanDetails"}>
+                            <Label htmlFor={'title'}>Title</Label>
+                            <Input type={'text'} id={'title'} onChange={this.handleChange} placeholder={'name your plan'}/>
+                        </div>
+                        <div className={"newPlanDetails"}>
+                            <Label>Notes</Label>
+                            <textarea id="content" onChange={this.handleChange} placeholder={'notes'}/>
+                        </div>
+                        <div className="newPlanDetails">
+                             <div  className="planCreator">
+                                 <Label htmlFor={'exercise'}>Exercise</Label>
+                                 <Input type={'text'} id={'exercise'} onChange={this.handleChange} placeholder={'exercise'}/>
+                             </div>
+
+                             <div  className="planCreator">
+                                 <Label htmlFor={'series'}>Series</Label>
+                                 <Input type={'text'} id={'series'} onChange={this.handleChange} placeholder={'give me number'}/>
+                             </div>
+                             <div  className="planCreator">
+                                 <Label htmlFor={'repeat'}>repeat</Label>
+                                 <Input type={'text'} id={'repeat'} onChange={this.handleChange} placeholder={'give me number'}/>
+                             </div>
+                             <div  className="planCreator">
+                                 <Label htmlFor={'weighted'}>weighted</Label>
+                                 <Input type={'text'} id={'weighted'} onChange={this.handleChange} placeholder={'kg'}/>
+                             </div>
+                             <div  className="planCreator">
+                                 <Label htmlFor={'brake'}>brake</Label>
+                                 <Input type={'text'} id={'brake'} onChange={this.handleChange} placeholder={'time brake'}/>
+                             </div>
+                        </div>
                     </FormGroup>
-                    <Button>Create Plan</Button>
+                    <Button className={"btn"}>Create Plan</Button>
                 </Form>
             </div>
         );
